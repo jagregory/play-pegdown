cf423768669fe72e183566693e8dafca25362b03
James Gregory <james@jagregory.com>
2011-12-07 21:24:37 +0000

    Make markdown extension output raw

--- app/exts/MarkdownExtensions.java
+++ app/exts/MarkdownExtensions.java
@@ -1,9 +1,10 @@
 package exts;
 
+import play.templates.BaseTemplate;
 import play.templates.JavaExtensions;
 
 public class MarkdownExtensions extends JavaExtensions {
-    public static String markdown(String markdown) {
-        return Markdown.toHtml(markdown);
+    public static BaseTemplate.RawData markdown(String markdown) {
+        return JavaExtensions.raw(Markdown.toHtml(markdown));
     }
 }
