0a91b28df1439753089dd43268ef0faa74811c31
James Gregory <james@jagregory.com>
2011-12-07 14:24:33 +0000

    Add whitespace trimming to the #{markdown} tag
    This prevents the situation where any indented markdown blocks would
    always be rendered as code-blocks due to their initial indentation.
    
    By default all #{markdown} blocks (but not the extensions) will trim
    the minimal amount of whitespace necessary to dedent the input. To
    disable this feature you can pass the `preserveWhitespace:true` option
    to the tag.
    
    For example:
    
        #{markdown preserveWhitespace:true}
                This whitespace will be preserved.
        #{/markdown}

--- app/exts/MarkdownFastTags.java
+++ app/exts/MarkdownFastTags.java
@@ -7,12 +7,67 @@ import play.templates.JavaExtensions;
 
 import java.io.PrintWriter;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MarkdownFastTags extends FastTags {
+    final static Pattern WHITESPACE = Pattern.compile("^([\\s]+).*");
+
     public static void _markdown(Map args, Closure body, PrintWriter out, GroovyTemplate.ExecutableTemplate template, int fromLine) {
         String markdown = JavaExtensions.toString(body);
+        Object preserveWhitespace = args.get("preserveWhitespace");
+
+        if (preserveWhitespace == null || preserveWhitespace.equals(false)) {
+            int indentToTrim = getMinimumIndentLevel(markdown);
+
+            markdown = dedent(markdown, indentToTrim);
+        }
+        
         String html = Markdown.toHtml(markdown);
 
         out.print(html);
+    }
+
+    static int getMinimumIndentLevel(String input) {
+        String[] lines = input.split("\\r?\\n|\\r");
+        
+        int indent = Integer.MAX_VALUE;
+
+        for (String line : lines) {
+            Matcher m = WHITESPACE.matcher(line);
+
+            if (m.find()) {
+                String whitespace = m.group(1);
+                
+                if (whitespace.length() == line.length()) {
+                    continue; // skip whitespace lines
+                }
+
+                if (indent > whitespace.length()) {
+                    indent = whitespace.length();
+                }
+            }
+        }
+        
+        return indent;
+    }
+
+    static String dedent(String input, int amount) {
+        if (amount == 0) {
+            return input;
+        }
+
+        String[] lines = input.split("\\r?\\n|\\r");
+        StringBuilder sb = new StringBuilder(input.length());
+        
+        for (String line : lines) {
+            sb.append(ltrim(line, amount));
+        }
+        
+        return sb.toString();
+    }
+
+    static String ltrim(String input, int amount) {
+        return input.replaceAll("^[\\s]{0,"+amount+"}(.*)", "$1");
     }
 }
